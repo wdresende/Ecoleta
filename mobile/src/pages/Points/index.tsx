799d3b8492292aab714c4c2a2ea593e23bed6e9d
Diego Resende <wdresende@gmail.com>
2020-06-28 17:36:32 -0300

    crate home page with routes

---
+++ mobile/src/pages/Points/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Points = () => {
+    return (
+        <View>
+            <Text>Points</Text>
+        </View>
+    )
+}
+
+export default Points;
